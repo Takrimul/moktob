4ac9bed06c9604b308f21f211f1d96a2a9dd5bde
tls-081 <takrimul.hasan@ticonsys.com>
2025-10-16 12:47:58 +0600

    feat(auth): switch to BCryptPasswordEncoder for improved password security

--- src/main/java/com/moktob/config/CustomPasswordEncoder.java
+++ src/main/java/com/moktob/config/CustomPasswordEncoder.java
@@ -1,26 +1,20 @@
 package com.moktob.config;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.crypto.password.Sha512PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CustomPasswordEncoder implements PasswordEncoder {
-    private final Sha512PasswordEncoder sha512 = new Sha512PasswordEncoder();
+    private final BCryptPasswordEncoder bcrypt = new BCryptPasswordEncoder(10);
 
     @Override
     public String encode(CharSequence rawPassword) {
-        return CoreUtils.password.encode(rawPassword);
+        return bcrypt.encode(rawPassword);
     }
 
     @Override
     public boolean matches(CharSequence rawPassword, String encodedPassword) {
-        // sha512
-        if (CoreUtils.string.length(encodedPassword) >= 128) {
-            return sha512.matches(rawPassword, encodedPassword);
-        }
-
-        // BCrypt
-        return CoreUtils.password.compare(rawPassword, encodedPassword);
+        return bcrypt.matches(rawPassword, encodedPassword);
     }
 }
